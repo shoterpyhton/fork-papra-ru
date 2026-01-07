396e80d05b51a4ec445334b0d5a7ae1e33e4d37a
shoterpyhton <167325821+shoterpyhton@users.noreply.github.com>
2026-01-07 20:55:02 +0300

    change button

--- apps/papra-client/src/modules/organizations/pages/organization.page.tsx
+++ apps/papra-client/src/modules/organizations/pages/organization.page.tsx
@@ -54,7 +54,7 @@ export const OrganizationPage: Component = () => {
                 <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-12">
 
                   <Button onClick={promptImport} class="h-auto items-start flex-col gap-4 py-4 px-6 text-left">
-                    <div class="i-tabler-upload size-6" />
+                    <div class="i-tabler-arrow-down size-6" />
 
                     {t('organizations.details.upload-documents')}
                   </Button>
